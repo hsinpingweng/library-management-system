f1ac9fd23edf5f9a3eee2c7ffd9879126b8b5fc5
hsinpingweng <39984087+hsinpingweng@users.noreply.github.com>
2021-01-24 12:19:42 +0000

    Revert "Feature add author page"

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into category (name) values('Romance');
-insert into publisher (name) values('Publisher A');
-insert into author (name, description) values('Tom', 'best writer');
-insert into book (name, description, isbn, author_id, category_id, publisher_id) values('Book 1', 'Description 1', '0000000000000', 1, 1, 1);
--- src/main/resources/static/js/app.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-
-    $("a.confirmDeletion").click( function() {
-        if (!confirm("Please confirm the deletion.")) return false;
-    });
-});
