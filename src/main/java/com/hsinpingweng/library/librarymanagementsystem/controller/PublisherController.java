404f9b90912131db3f1c0a06d495b1e3974cb17d
wenghsinping <ohmyhappy77@gmail.com>
2021-01-24 17:05:45 +0000

    create publisher edit page

--- src/main/java/com/hsinpingweng/library/librarymanagementsystem/controller/PublisherController.java
+++ src/main/java/com/hsinpingweng/library/librarymanagementsystem/controller/PublisherController.java
@@ -6,11 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -55,6 +54,40 @@ public class PublisherController {
         }
 
         return "redirect:/publishers/add";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model){
+
+        Publisher publisher = publisherRepo.getOne(id);
+        model.addAttribute("publisher", publisher);
+        return "publisher/edit";
+    }
+
+
+    @PostMapping("/edit/{id}")
+    public String edit(@PathVariable int id, @Valid Publisher publisher, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model){
+
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("publisherName", publisher.getName());
+            return "publisher/edit";
+        }
+
+        Publisher publisherExist = publisherRepo.findByName(publisher.getName());
+
+        if(publisherExist != null) {
+            // add value in session
+            redirectAttributes.addFlashAttribute("message", "Publisher exists!");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+        } else {
+            redirectAttributes.addFlashAttribute("message", "Publisher edited!");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+            publisher.setId(id);
+            publisherRepo.save(publisher);
+        }
+
+        return "redirect:/publishers/edit/" + id;
     }
 
 }
