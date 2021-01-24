50c8a15b3ff77c0b56833b4f7e0ee86602514741
wenghsinping <ohmyhappy77@gmail.com>
2021-01-24 15:40:29 +0000

    create publisher add page

--- src/main/java/com/hsinpingweng/library/librarymanagementsystem/controller/PublisherController.java
+++ src/main/java/com/hsinpingweng/library/librarymanagementsystem/controller/PublisherController.java
@@ -5,8 +5,11 @@ import com.hsinpingweng.library.librarymanagementsystem.repository.PublisherRepo
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -23,6 +26,35 @@ public class PublisherController {
         model.addAttribute("publishers", publishers);
 
         return "publisher/index";
+    }
+
+    @GetMapping("/add")
+    public String add(Publisher publisher){
+
+        return "publisher/add";
+    }
+
+    @PostMapping("/add")
+    public String add(Publisher publisher, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+
+        if(bindingResult.hasErrors()) return "publisher/add";
+
+        Publisher publisherExist = publisherRepo.findByName(publisher.getName());
+        if (publisherExist != null) {
+
+            redirectAttributes.addFlashAttribute("message", "Publisher exists!");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            // add value in session
+            redirectAttributes.addFlashAttribute("publisherInfo", publisher);
+
+        } else {
+            redirectAttributes.addFlashAttribute("message", "publisher added");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+            publisherRepo.save(publisher);
+        }
+
+        return "redirect:/publishers/add";
     }
 
 }
