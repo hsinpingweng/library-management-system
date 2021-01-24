1ff47bc0d647c0d9a4c63020746ce7660fb7ab11
wenghsinping <ohmyhappy77@gmail.com>
2021-01-24 23:58:24 +0000

    add book index page deletion function

--- src/main/java/com/hsinpingweng/library/librarymanagementsystem/controller/BookController.java
+++ src/main/java/com/hsinpingweng/library/librarymanagementsystem/controller/BookController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -81,5 +82,13 @@ public class BookController {
         return "redirect:/books/add";
     }
 
-    
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable int id){
+
+        //TODO - Check foreign key constraint violation before deletion
+        bookRepo.deleteById(id);
+
+        return "redirect:/books";
+    }
 }
